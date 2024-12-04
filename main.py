1ab7c70fbcb1b05f1c1844f8ea8746b1f8a8728c
sunny <sunny.kumar@brihaspatitech.com>
2024-12-04 17:40:10 +0530

    large file issue

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ def home():
                            movie_titles=movie_titles)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=7676)
+    app.run(debug=False, host='0.0.0.0', port=7676)
 
